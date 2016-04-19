1b48745d7f63cac913dccf5eed7bf469ba7efc87
asmaaelk <asmaa.elkeurti@gmail.com>
2016-04-19 01:06:55 +0000

    first final touches

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, redirect
 import thread, time
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
 ## Raspberry 2 GPIO pin numbers. 
 pin_numbers = [3, 5, 7, 8, 10, 11, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26]
@@ -12,14 +12,14 @@ pin_numbers = [3, 5, 7, 8, 10, 11, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26]
 frequency = 50
 
 ## Bool to control when LEDs are active
-LED_active = false
+LED_active = False
 
 ## Setup pwm data structure. Keys are pins. Values
 ##  are tuple of (duty cycle value, pwm reference)  
 def pwm_setup():
     pins = {}
     for pin_num in pin_numbers:
-        pins[pin_num] = [0]
+        GPIO.setup(pin_num, GPIO.OUT)
         pins[pin_num] = [0, GPIO.PWM(pin_num, frequency)]
         pins[pin_num][1].start(0)
     return(pins)
@@ -33,16 +33,10 @@ def pwm_cleanup(pins):
 ## Define pwm update loop which will run other thread
 def pwm_loop():
     global pins
-    while True
+    while True:
         for pin in pins:
-            print(pin, pins[pin])
-            # pin.ChangeDutyCycle(pins[pin])
+            pins[pin][1].ChangeDutyCycle(pins[pin][0])
         time.sleep(0.1)
-
-## Updates pwm values for individual LEDs. Called when user hits submit.
-def update_pwm(index, brightness):
-    global pins
-    pins[index] = brightness
 
 ## Start setup
 pins = pwm_setup()
@@ -63,12 +57,13 @@ def update_pwm():
     if request.method == 'POST':
         index = int(request.form.get('Index'))
         brightness = int(request.form.get('Brightness'))
-        
-        update_pwm(index, brightness)
+
+        # Update the brightness
+        pins[index][0] = brightness
         
         return( redirect ('/') )
     elif request.method == 'GET':
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0')
